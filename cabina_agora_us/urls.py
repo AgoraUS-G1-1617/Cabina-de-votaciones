353aabfa596afb011854d49aac4ed1da2ff32c84
Juanlu <jlljunior_90@hotmail.com>
2016-11-29 19:50:46 +0100

    corrección de urls, ahora cualquier url distinta de /cabinarecepcion y /id_poll/ llevará a la página principal mostrando un error.

--- cabina_agora_us/urls.py
+++ cabina_agora_us/urls.py
@@ -3,13 +3,16 @@ from django.conf.urls import patterns, include, url
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
-from cabina_app.views import cabinarecepcion ,recibe_id_votacion_fromdb
+from cabina_app.views import cabinarecepcion ,recibe_id_votacion, recibe_id_votacion_fromdb
 
 
 
 urlpatterns = patterns('',
-                       url(r'^', include('cabina_app.urls')),
-                       url(r'^cabinarecepcion/', cabinarecepcion),
+                       #url(r'^/$', include('cabina_app.urls')),
+                       #url(r'^(?P<id_poll>\d+)/$',recibe_id_votacion),
+                       url(r'^cabinarecepcion/$', cabinarecepcion),
+                       url(r'', include('cabina_app.urls')),
+
 
                        #Simula la renderizacion de un index a partir de un id de Question
                        url(r'^votacion/(?P<id_question>\w+)/$', recibe_id_votacion_fromdb),
--- cabina_app/views.py
+++ cabina_app/views.py
@@ -6,41 +6,43 @@ from cabina_app.dbConnect import *
 
 
 @api_view(['GET'])
-def recibe_id_votacion(request, id_poll):
-
-    # Comprobar que el identificador de la votacion es numerico
-    try:
-        id_poll = int(id_poll)
-    except ValueError:
-        informacion = "El identificador de la votación es erronea"
-        return render(request, "informacion.html", {'informacion': informacion, 'error': True})
-
-    # Comprobar que dicho usuario autenticado es valido
-    if not verify_user(request):
-        informacion = "El usuario es erroneo, autenticate de nuevo"
-    # Comprobar que el usuario autenticado puede votar en dicha votacion
-    elif not can_vote(request, id_poll):
-        informacion = "Usted no puede votar en esta votación"
-    else:
-        # Construir la votacion
-        poll = get_poll(id_poll)
-        if poll is None:
+def recibe_id_votacion(request, id_poll = None):
+        if(id_poll is None):
+            informacion = "No se ha recibido ninguna votacion"
+            return render(request, "informacion.html", {'informacion': informacion, 'error': True})
+        # Comprobar que el identificador de la votacion es numerico
+        try:
+            id_poll = int(id_poll)
+        except ValueError:
             informacion = "El identificador de la votación es erronea"
+            return render(request, "informacion.html", {'informacion': informacion, 'error': True})
 
-    if informacion is None:
-        respuesta = render(request, "index.html", {'poll': poll, 'questions': poll.questions})
-    else:
-        respuesta =  render(request, "informacion.html", {'informacion': informacion, 'error': True})
-    return respuesta
+        # Comprobar que dicho usuario autenticado es valido
+        if not verify_user(request):
+            informacion = "El usuario es erroneo, autenticate de nuevo"
+        # Comprobar que el usuario autenticado puede votar en dicha votacion
+        elif not can_vote(request, id_poll):
+            informacion = "Usted no puede votar en esta votación"
+        else:
+            # Construir la votacion
+            poll = get_poll(id_poll)
+            if poll is None:
+                informacion = "El identificador de la votación es erronea"
+
+        if informacion is None:
+            respuesta = render(request, "index.html", {'poll': poll, 'questions': poll.questions})
+        else:
+            respuesta =  render(request, "informacion.html", {'informacion': informacion, 'error': True})
+        return respuesta
 
 'Simula la renderización de index mediante un poll que viene de base de datos'
 'actualmente, dado que Poll no tiene atributo questions y que éste está en Question, lo que se recupera es una pregunta de la Poll'
 'This is for developers tests purposes only'
 @api_view(['GET'])
 def recibe_id_votacion_fromdb(request, id_question):
-    question = get_Question(id_question)
-    #poll = get_Poll(id_poll)
-    return render(request, "index.html", {'poll': question.questions, 'questions': [question]})
+    #question = get_Question(id_question)
+    poll = get_Poll(id_question)
+    return render(request, "index.html", {'poll': poll, 'questions': poll.question_set.all()})
 
 
 
@@ -101,4 +103,3 @@ def cabinarecepcion(request):
         informacion = "Lo sentimos, el metodo solicitado no esta disponible"
 
     return render(request, "informacion.html", {'informacion': informacion, 'error': error})
-
