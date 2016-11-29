47ca828319000bca800a5dec89f1fa316e5012d6
Juanlu <jlljunior_90@hotmail.com>
2016-11-29 20:02:38 +0100

    corrección de url de prueba votacion/id_poll

--- cabina_agora_us/urls.py
+++ cabina_agora_us/urls.py
@@ -10,12 +10,13 @@ from cabina_app.views import cabinarecepcion ,recibe_id_votacion, recibe_id_vota
 urlpatterns = patterns('',
                        #url(r'^/$', include('cabina_app.urls')),
                        #url(r'^(?P<id_poll>\d+)/$',recibe_id_votacion),
+                       #Simula la renderizacion de un index a partir de un id de Question
+                       url(r'^votacion/(?P<id_question>\w+)/$', recibe_id_votacion_fromdb),
                        url(r'^cabinarecepcion/$', cabinarecepcion),
                        url(r'', include('cabina_app.urls')),
 
 
-                       #Simula la renderizacion de un index a partir de un id de Question
-                       url(r'^votacion/(?P<id_question>\w+)/$', recibe_id_votacion_fromdb),
+
 
                        #  simula la url de administracion de votacion
                        # url(r'^prueba_id_votacion/(?P<id_votacion>\d+)/$', prueba_id_votacion),
