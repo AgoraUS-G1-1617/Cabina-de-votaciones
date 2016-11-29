7c47091ed0e9ec46e887686cd51b4ad1af6fce97
Juanlu <jlljunior_90@hotmail.com>
2016-11-29 18:28:50 +0100

    cambio de url básico de /cabinaus/home  a /home

--- cabina_agora_us/urls.py
+++ cabina_agora_us/urls.py
@@ -8,11 +8,11 @@ from cabina_app.views import cabinarecepcion ,recibe_id_votacion_fromdb
 
 
 urlpatterns = patterns('',
-                       url(r'^cabinaus/', include('cabina_app.urls')),
+                       url(r'^', include('cabina_app.urls')),
                        url(r'^cabinarecepcion/', cabinarecepcion),
 
                        #Simula la renderizacion de un index a partir de un id de Question
-                       #url(r'^votacion/(?P<id_question>\w+)/$', recibe_id_votacion_fromdb),
+                       url(r'^votacion/(?P<id_question>\w+)/$', recibe_id_votacion_fromdb),
 
                        #  simula la url de administracion de votacion
                        # url(r'^prueba_id_votacion/(?P<id_votacion>\d+)/$', prueba_id_votacion),
